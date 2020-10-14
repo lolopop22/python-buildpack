ef67244b0ce9dc972c5441301775e4ea4e041ec7
loic.assontia.kengni <loic.assontia@auf.org>
2020-10-14 15:45:44 +0200

    modif du fichier install_tools.sh

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
-
+echo "------> Installing Rclone via install_tools.sh"
+go get github.com/rclone/rclone
 go install github.com/onsi/ginkgo/ginkgo
 
 if [ ! -f .bin/buildpack-packager ]; then
